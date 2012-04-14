e23528b5619eaa5ff845814ee7ec46b035e6554a
Conn O'Griofa <connogriofa@gmail.com>
2012-04-15 00:03:25 +0100

    board-msm7x25: add more devices as multitouch-enabled

--- arch/arm/mach-msm/board-msm7x25.c
+++ arch/arm/mach-msm/board-msm7x25.c
@@ -3952,7 +3952,9 @@ int board_surport_fingers(bool * is_surport_fingers)
 
     if( machine_is_msm7x25_u8500() || machine_is_msm7x25_um840() \
      || machine_is_msm7x25_u8350() || machine_is_msm7x25_u8130() \
-	 || machine_is_msm7x25_c8510() )
+	 || machine_is_msm7x25_c8510() || machine_is_msm7x25_u8160() \
+     || machine_is_msm7x25_c8500() || machine_is_msm7x25_c8600() \
+	 || machine_is_msm7x25_u8150() || machine_is_msm7x25_c8150())
     {
          *is_surport_fingers = true;
     }
